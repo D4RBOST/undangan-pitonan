908f7d8ca0d924a0fe68909a5f3c45e5d4e6a082
DARBOST <arfentriyahya@gmail.com>
2026-04-07 21:37:41 +0700

    first commit undangan pitonan

---
+++ config.js
@@ -0,0 +1,37 @@
+const CONFIG = {
+  // Baby Information
+  babyName: "Khayruzan Nalendra Yahya",
+  parentsNames: "Arfen & Zhava",
+
+  // Event Date & Time
+  eventDate: "2026-05-10T06:00:00", // ISO Format for Countdown
+  displayDate: "Minggu, 10 Mei 2026",
+  javaneseDate: "22 Zulkaidah 1957",
+  eventTime: "06.00 - Selesai WIB",
+
+  // Location
+  locationName: "Kediaman Keluarga",
+  locationAddress: "Kiping, Kabupaten Tulungagung, Jawa Timur",
+  mapsLink: "https://maps.app.goo.gl/vU7LGdUSkksKhtdz8",
+  mapsEmbedUrl:
+    "https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3950.24096451719!2d111.849475!3d-8.076889!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zOMKwMDQnMzYuOCJTIDExMcKwNTAnNTguMSJF!5e0!3m2!1sid!2sid!4v1770825617073!5m2!1sid!2sid",
+
+  // Media
+  musicUrl: "./lagujawa.mp3",
+  // mainPhotoUrl: "http://static.photos/minimal/640x800/123",
+  mainPhotoUrl: "./foto1.png",
+
+
+  // Gifts
+  bankAccount: "1410027597392",
+  bankOwner: "Arfen Tri Handaru",
+  giftAddress: "Kiping RT 01 RW 01 Gondang, Tulungagung",
+
+  // Dress Code Colors (Optional Metadata)
+  dressCode: [
+    { name: "Hijau Lumut", color: "#2D5016" },
+    { name: "Emas", color: "#D4AF37" },
+    { name: "Krem", color: "#FAF7F2" },
+    { name: "Coklat", color: "#8B4513" },
+  ],
+};
